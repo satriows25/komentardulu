041013a290d27351fd1ba689eba8e424f3209cf9
Satrio Wicaksono <88221074+satriows25@users.noreply.github.com>
2022-06-15 21:20:06 +0700

    Menambah firestore, chakra-ui, dan absolute import (#3)
    Menambah firestore, chakra-ui, dan absolute import
    
    PR selesai #3

---
+++ styles/theme.js
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    body: `'Inter', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 600,
+    bold: 700,
+  },
+});
+
+export default theme;
